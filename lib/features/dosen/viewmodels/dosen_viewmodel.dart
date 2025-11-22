1142db013218e6d4e20c148e151dc55ef4b83694
Aldo Khrisna <aldokhrisna@gmail.com>
2025-11-22 11:13:47 +0700

    feat: add profile app bar & logout modal confirmation (#5)
    * feat: Implementasi wrapper untuk pengecekan status onboarding pada rute
    
    * feat(dosen): add bottom navbar & modal bottom sheet
    
    * chore: change application icon
    
    * refactor: remove unnecessary wrapper
    
    * feat: update profile AppBar and add logout confirmation modal

---
+++ lib/features/dosen/viewmodels/dosen_viewmodel.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../../auth/views/login_page.dart';
+
+class DosenViewModel extends ChangeNotifier {
+  // Logic logout (hapus token dll)
+  Future<void> logout() async {
+    await Future.delayed(const Duration(milliseconds: 500));
+  }
+
+  // Handle logout + navigate ke login
+  Future<void> handleLogout(BuildContext context) async {
+    await logout();
+
+    Navigator.of(context).pushAndRemoveUntil(
+      MaterialPageRoute(builder: (_) => const LoginPage()),
+      (route) => false,
+    );
+  }
+}
