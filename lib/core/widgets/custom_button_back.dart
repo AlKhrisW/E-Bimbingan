1841577327ef1bfde5a29bd2313429ab0b5db63c
afgan galih <afgangalihf@gmail.com>
2025-11-23 18:01:23 +0700

    fix(admin): rev custom_back_button

--- lib/core/widgets/custom_button_back.dart
+++ lib/core/widgets/custom_button_back.dart
@@ -5,29 +5,22 @@ import 'package:flutter/services.dart';
 import '../themes/app_theme.dart';
 
 class CustomBackButton extends StatelessWidget {
-  final double size;
-
-  const CustomBackButton({
-    super.key,
-    this.size = 24, // default size biar tidak error
-  });
+  const CustomBackButton({super.key});
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      padding: const EdgeInsets.only(left: 16),
+      padding: EdgeInsets.only(left: 16),
       icon: Container(
-        width: size + 10,
-        height: size + 10,
+        padding: EdgeInsets.all(3),
         decoration: BoxDecoration(
           color: AppTheme.primaryColor,
           shape: BoxShape.circle,
         ),
         child: Center(
           child: Icon(
-            Icons.arrow_back_ios_new,
-            color: Colors.white,
-            size: size * 0.7,
+            Icons.chevron_left, 
+            color: Colors.white, 
           ),
         ),
       ),
