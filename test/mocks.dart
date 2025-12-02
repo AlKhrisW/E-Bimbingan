1ef8fc7019ff9e6473c36d9fd6be0fa0f7cc1d45
afgan galih <afgangalihf@gmail.com>
2025-12-02 21:40:32 +0700

    feat: unit testing -> admin_user_management_vm & auth_vm

---
+++ test/mocks.dart
@@ -0,0 +1,21 @@
+import 'package:mockito/annotations.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+// --- SERVICE LAYER PROYEK ANDA ---
+import 'package:ebimbingan/data/services/user_service.dart'; 
+import 'package:ebimbingan/data/services/firestore_service.dart'; 
+import 'package:ebimbingan/data/services/firebase_auth_service.dart'; 
+
+// --- DEFINISI CLASS YANG AKAN DI-MOCK ---
+@GenerateMocks([
+  // Services
+  UserService, 
+  FirestoreService, 
+  FirebaseAuthService, 
+  
+  // Firebase Classes (harus di-mock)
+  FirebaseAuth,     
+  UserCredential,   
+  User,             
+]) 
+void main() {}
