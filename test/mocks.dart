91e0bc4348e64e3101853ebad9285cfff9da7785
afgan galih <afgangalihf@gmail.com>
2025-12-03 12:21:25 +0700

    feat: unit test detail mapping

--- test/mocks.dart
+++ test/mocks.dart
@@ -1,21 +1,21 @@
-import 'package:mockito/annotations.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+  import 'package:mockito/annotations.dart';
+  import 'package:firebase_auth/firebase_auth.dart';
 
-// --- SERVICE LAYER PROYEK ANDA ---
-import 'package:ebimbingan/data/services/user_service.dart'; 
-import 'package:ebimbingan/data/services/firestore_service.dart'; 
-import 'package:ebimbingan/data/services/firebase_auth_service.dart'; 
+  // --- SERVICE LAYER PROYEK ANDA ---
+  import 'package:ebimbingan/data/services/user_service.dart'; 
+  import 'package:ebimbingan/data/services/firestore_service.dart'; 
+  import 'package:ebimbingan/data/services/firebase_auth_service.dart'; 
 
-// --- DEFINISI CLASS YANG AKAN DI-MOCK ---
-@GenerateMocks([
-  // Services
-  UserService, 
-  FirestoreService, 
-  FirebaseAuthService, 
-  
-  // Firebase Classes (harus di-mock)
-  FirebaseAuth,     
-  UserCredential,   
-  User,             
-]) 
-void main() {}
+  // --- DEFINISI CLASS YANG AKAN DI-MOCK ---
+  @GenerateMocks([
+    // Services
+    UserService, 
+    FirestoreService, 
+    FirebaseAuthService, 
+    
+    // Firebase Classes (harus di-mock)
+    FirebaseAuth,     
+    UserCredential,   
+    User,             
+  ]) 
+  void main() {}
